e882b42a8a3da41b5fdcc66d0462451cd8443514
PIOTR NOWAK <the.real.b.rabbid@gmail.com>
2022-03-15 09:02:26 +0100

    virtual reality beta

---
+++ script.js
@@ -0,0 +1,19 @@
+const user = document.getElementById('User');
+const colorInfo = document.getElementById('favColor');
+const petInfo = document.getElementById('favPet');
+const cityInfo = document.getElementById('cityInfo');
+const jobInfo = document.getElementById('jobStatus');
+
+const userText = "John";
+const userColor = "Zielony";
+const userPet = "Pies";
+const cityInfo = "Nie";
+
+
+user.textContent = userText;
+colorInfo.textContent = userColor;
+petInfo.textContent = userPet;
+cityInfo.textContent = cityInfo;
+
+
+colorInfo.style.color = "green";
